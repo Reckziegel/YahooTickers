08c9cdbbfbe0ff35c921621633c7e057f9987c43
Bernardo <bernardo_cse@hotmail.com>
2018-12-17 15:37:34 -0200

    fix #6.

--- tests/testthat/test_get_forecasts.R
+++ tests/testthat/test_get_forecasts.R
@@ -5,7 +5,9 @@ library(tidyr)
 
 context("Testing get_forecasts()")
 
-stocks <- tibble(
+set.seed(1)
+
+stock_fc <- tibble(
   time = as.Date('2009-01-01') + 0:49,
   X    = rnorm(50, 0, 1),
   Y    = rnorm(50, 0, 2),
@@ -17,27 +19,25 @@ stocks <- tibble(
              .initial    = 40,
              .assess     = 1,
              .cumulative = TRUE,
-             .fun        = auto.arima,
-             seasonal    = FALSE,
-             stationary  = TRUE,
-             max.p       = 1,
-             max.q       = 1
+             .fun        = Arima,
+             order       = c(0, 0, 0),
+             seasonal    = c(0, 0, 0)
              ) %>%
   get_forecasts()
 
 test_that("The output contains the correct dimmensions", {
 
   # class
-  expect_is(stocks, "tbl")
+  expect_is(stock_fc, "tbl")
 
   # rows
-  expect_equal(nrow(stocks), 30)
+  expect_equal(nrow(stock_fc), 30)
 
   # cols
-  expect_equal(ncol(stocks), 8)
+  expect_equal(ncol(stock_fc), 8)
 
   # names
-  expect_equal(names(stocks),
+  expect_equal(names(stock_fc),
                c("time", "stock", "price", "mean_forecast", "lo.80", "lo.95", "hi.80", "hi.95")
   )
 
